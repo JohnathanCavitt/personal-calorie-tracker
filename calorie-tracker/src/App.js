d22600bc92ccc09787e9feaa5737afeb3991ba4a
Johnathan Cavitt <johnathancavitt@gmail.com>
2021-11-03 16:21:57 -0700

    huge updates

--- calorie-tracker/src/App.js
+++ calorie-tracker/src/App.js
@@ -3,24 +3,52 @@ import React, { useState } from "react";
 import Header from "./Header.js";
 
 function App() {
-  let test = [
-    { food: "burger", calories: "380" },
-    { food: "veal", calories: "380" },
-    { food: "fries", calories: "380" },
-  ];
+  let test = JSON.parse(localStorage.getItem("foodList"));
+  if (test === null) {
+    test = [];
+  }
+  console.log(test);
+  // let test = [
+  //   { food: "burger", calories: "380" },
+  //   { food: "veal", calories: "380" },
+  //   { food: "fries", calories: "380" },
+  // ];
 
+  const [showForm, setShowForm] = useState(false);
   const [data, setData] = useState(test);
+  const [foodName, setFoodName] = useState("");
+  const [calorieAmount, setCalorieAmount] = useState("");
 
   const getData = (newData) => {
     setData(newData);
   };
 
-  const addItem = (e, data) => {
-    let obj = { food: "pizza", calories: "300" };
-    let newData = [...data, obj];
-    //newData.push(obj);
-    console.log(data);
-    getData(newData);
+  const formToggle = (e) => {
+    if (showForm === false) {
+      setShowForm(true);
+    } else if (showForm === true) {
+      setShowForm(false);
+    }
+  };
+
+  const handleSubmit = (food = "", calories = "") => {
+    let obj = { food: food, calories: calories };
+    let newList = [...data, obj];
+    setData(newList);
+    console.log(newList);
+    localStorage.setItem("foodList", JSON.stringify(newList));
+    formToggle();
+  };
+  let totalCalories = 0;
+  for (let i = 0; i < data.length; i++) {
+    let calCurr = data[i]["calories"];
+    calCurr = parseInt(calCurr, 10);
+    isNaN(calCurr) ? (calCurr = 0) : (totalCalories += calCurr);
+  }
+
+  const handleClear = () => {
+    setData([]);
+    localStorage.clear();
   };
 
   return (
@@ -28,20 +56,51 @@ function App() {
       <button
         id="add-button"
         onClick={(e) => {
-          addItem(e, data);
+          formToggle(e);
         }}
       >
         Add Item
       </button>
+      {showForm ? (
+        <div id="form-container">
+          <form id="input-form">
+            <div className="input-bar">
+              <input
+                type="text"
+                placeholder="Enter food name"
+                className="input-field"
+                onChange={(e) => setFoodName(e.target.value)}
+              />
+            </div>
+            <div className="input-bar">
+              <input
+                type="text"
+                placeholder="Enter calorie amount"
+                className="input-field"
+                onChange={(e) => setCalorieAmount(e.target.value)}
+              />
+            </div>
+            <input
+              type="submit"
+              id="submit-button"
+              onClick={(e) => handleSubmit(foodName, calorieAmount)}
+            />
+          </form>
+        </div>
+      ) : null}
       <div className="container">
         {data.map((item, index) => {
           return (
-            <Header key={index} container={item} />
+            <Header key={index} container={item} data={getData} />
             // <h1 key={index}>
             //   {item["food"]} : {item["calories"]}
             // </h1>
           );
         })}
+        <p>total calories = {totalCalories}</p>
+        <button id="clear" onClick={handleClear}>
+          clear items
+        </button>
       </div>
     </div>
   );
--- calorie-tracker/src/Header.js
+++ calorie-tracker/src/Header.js
@@ -2,7 +2,30 @@ import React from "react";
 import "./Header.css";
 
 const Header = (props) => {
-  return <h1>{JSON.stringify(props.container)}</h1>;
+  const handleRemove = () => {
+    let retData = JSON.parse(localStorage.getItem("foodList"));
+    for (let i = 0; i < retData.length; i++) {
+      if (
+        retData[i]["food"] === props.container["food"] &&
+        retData[i]["calories"] === props.container["calories"]
+      ) {
+        retData.splice(i, 1);
+        break;
+      }
+    }
+    localStorage.setItem("foodList", JSON.stringify(retData));
+    props.data(retData);
+  };
+  return (
+    <div className="items">
+      <button className="remove" onClick={handleRemove}>
+        -
+      </button>
+      <h1>
+        {props.container["food"]} : {props.container["calories"]}
+      </h1>
+    </div>
+  );
 };
 
 export default Header;
